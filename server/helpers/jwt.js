fcfbcd2f9355e6e292d28751b88a635b9d1e5341
Luiza <luitays@mail.ru>
2019-11-11 13:21:56 +0300

    try to fix prod error

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -6,7 +6,8 @@ function jwt() {
     path: [
       // public routes that don't require authentication
       '/users/authenticate',
-      '/users/register'
+      '/users/register',
+      '/favicon.ico'
     ]
   });
 }
